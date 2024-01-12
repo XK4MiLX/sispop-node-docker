7d8d3b68f6ce5c8edfe05df3ad7ad395ed0cac1b
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-12 12:38:55 +0100

    added logs

--- storage_init.sh
+++ storage_init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo -e "Initial awaiting.."
 sleep 120
+echo -e "Starting storage.."
+echo -e "----------------------------------------------------------------------------------------------------"
 sispop-storage 0.0.0.0 22020 --data-dir=/root/.sispop/storage --sispopd-rpc-port=30000
