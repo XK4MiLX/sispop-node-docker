aaa9393978a350366773fce6d3e14a58ee2d8e6b
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-12 11:30:54 +0100

    Create sispop_init.sh

---
+++ sispop_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+url_array=(
+    "https://api4.my-ip.io/ip"
+    "https://checkip.amazonaws.com"
+    "https://api.ipify.org"
+)
+
+function get_ip() {
+    for url in "$@"; do
+        WANIP=$(curl --silent -m 15 "$url" | tr -dc '[:alnum:].')
+        # Remove dots from the IP address
+        IP_NO_DOTS=$(echo "$WANIP" | tr -d '.')
+        # Check if the result is a valid number
+        if [[ "$IP_NO_DOTS" != "" && "$IP_NO_DOTS" =~ ^[0-9]+$ ]]; then
+            break
+        fi
+    done
+}
+
+get_ip "${url_array[@]}"
+sispopd --service-node --service-node-public-ip ${WANIP}--storage-server-port 22020 --seed-node 13.53.97.58
