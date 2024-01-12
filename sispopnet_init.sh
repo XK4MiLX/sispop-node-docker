0dcb1216432977aaa71b963cc2aae2cdaeb52a8b
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-12 17:15:52 +0100

    Update sispopnet_init.sh

--- sispopnet_init.sh
+++ sispopnet_init.sh
@@ -3,10 +3,14 @@ if [[ ! -d /root/.sispopnet ]]; then
   echo -e "Initial setup.."
   mkdir /root/.sispopnet
   cp /sispopnet.ini /root/.sispopnet/sispopnet.ini
+  cd /root/.sispopnet
   wget https://seed.sispop.site/bootstrap.signed
+  if [[ ! /root/.sispopnet/self.signed ]]; then
+    cp bootstrap.signed self.signed
+  fi
 fi
 echo -e "Awaiting.."
-sleep 240
+sleep 200
 echo -e "Starting sispopnet.."
 echo -e "----------------------------------------------------------------------------------------------------"
 sispopnet
