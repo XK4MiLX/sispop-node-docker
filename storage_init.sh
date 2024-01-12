69029b680104896a1611fba1edb9f6787a0a14b2
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-13 00:41:17 +0100

    added bypass

--- storage_init.sh
+++ storage_init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo -e "Awaiting.."
-sleep 120
+if [[ "$1" != "bypass" ]]; then
+  echo -e "Awaiting.."
+  sleep 120
+fi
 echo -e "Starting storage.."
 echo -e "----------------------------------------------------------------------------------------------------"
 sispop-storage 0.0.0.0 22020 --data-dir=/root/.sispop/storage --sispopd-rpc-port=30000
