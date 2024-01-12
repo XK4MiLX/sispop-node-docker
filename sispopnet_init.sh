1004241f8b013ed45f749ad5461744b92024b0ed
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-12 12:41:08 +0100

    added logs

--- sispopnet_init.sh
+++ sispopnet_init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 if [[ ! -d /root/.sispopnet ]]; then
+  echo -e "Initial setup.."
   mkdir /root/.sispopnet
   cp /sispopnet.ini /root/.sispopnet/sispopnet.ini
   wget https://seed.sispop.site/bootstrap.signed
 fi
+echo -e "Awaiting.."
 sleep 240
+echo -e "Starting sispopnet.."
+echo -e "----------------------------------------------------------------------------------------------------"
 sispopnet
