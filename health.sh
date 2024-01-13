607f4f405d94d186924989b89c04b47689039b3e
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-13 09:41:28 +0100

    added redirect to docker logs

--- health.sh
+++ health.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 extract_time() {
   time_str=$1
   value=$(echo "$time_str" | grep -oE '([0-9]+(\.[0-9]+)?)\s*(min|sec)' | sed -E 's/([0-9]+(\.[0-9]+)?)\s*(min|sec)/\1 \3/')
@@ -26,6 +25,7 @@ percentage=$(echo "$status" | grep -oP '(\d+\.\d+)%')
 sn_part=$(echo "$status" | grep -oP 'SN: \K.*' | cut -d' ' -f2-)
 output="$height ($percentage), $sn_part"
 echo -e "$output"
+echo -e "$output"  >> /proc/1/fd/1
 
 if [[ $output == "" ]]; then
  exit 1
@@ -38,15 +38,19 @@ sispopnet_time=$(extract_time "${time_parts[3]}")
 if [[ "$sispopnet_time" != "" && "$storage_time" != "" ]]; then
   if [[ "$sispopnet_time" -ge 305 ]]; then
     echo -e "Performing your action because Sispopnet last ping time is greater than 305 seconds."
+    echo -e "Performing your action because Sispopnet last ping time is greater than 305 seconds." >> /proc/1/fd/1
     supervisorctl restart sispopnet > /dev/null 2>&1
   else
     echo -e "Sispopnet last ping time is within acceptable range: $sispopnet_time seconds."
+    echo -e "Sispopnet last ping time is within acceptable range: $sispopnet_time seconds." >> /proc/1/fd/1
   fi
 
   if [[ "$storage_time" -ge 305 ]]; then
     echo "Performing your action because Storage last ping time is greater than 305 seconds."
+    echo "Performing your action because Storage last ping time is greater than 305 seconds." >> /proc/1/fd/1
     supervisorctl restart storage > /dev/null 2>&1
   else
     echo -e "Storage last ping time is within acceptable range: $storage_time seconds."
+    echo -e "Storage last ping time is within acceptable range: $storage_time seconds."  >> /proc/1/fd/1
   fi
 fi
